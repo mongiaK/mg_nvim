953dd40ae949852f7c3bc3846b4850b18b97aa5d
mongia <mongiaK@outlook.com>
2023-05-11 17:10:31 +0800

    change something

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,11 +1,33 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      close_if_last_window = true,
-      window = {
-        width = 30,
-      },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            close_if_last_window = true,
+            window = {
+                width = 30,
+            },
+        },
     },
-  },
+    {
+        "nvim-lualine/lualine.nvim",
+        enabled = true,
+    },
+    {
+        "folke/noice.nvim",
+        enabled = false,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            default = {
+                mode = { "n", "v" },
+            },
+        },
+        config = function(_, opts)
+            local wk = require("which-key")
+            wk.setup(opts)
+            wk.register(opts.default)
+        end,
+    },
 }
