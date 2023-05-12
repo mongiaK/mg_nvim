2f825e293bc027d9bbea838191e5940d66b86409
mongia <mongiaK@outlook.com>
2023-05-12 12:07:57 +0800

    add dap config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
     {
         "LazyVim/LazyVim",
         opts = {
-            -- colorscheme = "monokai",
-            colorscheme = "catppuccin",
+            colorscheme = "monokai",
+            --colorscheme = "catppuccin",
         },
     },
 }
