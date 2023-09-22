2aa2cd521d921550496de9e2078f4d27cbaca656
mongia <mongiaK@outlook.com>
2023-09-22 08:59:03 +0800

    add gutentags config

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -16,6 +16,90 @@ return {
     {
         "ludovicchabant/vim-gutentags",
         enabled = true,
+        config = function()
+            vim.g.gutentags_ctags_exclude = {
+                '*.git',
+                '*.svg',
+                '*.hg',
+                '*/tests/*',
+                'build',
+                'dist',
+                '*sites/*/files/*',
+                'bin',
+                'node_modules',
+                'bower_components',
+                'cache',
+                'compiled',
+                'docs',
+                'example',
+                'bundle',
+                'vendor',
+                '*.md',
+                '*-lock.json',
+                '*.lock',
+                '*bundle*.js',
+                '*build*.js',
+                '.*rc*',
+                '*.json',
+                '*.min.*',
+                '*.map',
+                '*.bak',
+                '*.zip',
+                '*.pyc',
+                '*.class',
+                '*.sln',
+                '*.Master',
+                '*.csproj',
+                '*.tmp',
+                '*.csproj.user',
+                '*.cache',
+                '*.pdb',
+                'tags*',
+                'cscope.*',
+                -- '*.css',
+                -- '*.less',
+                -- '*.scss',
+                '*.exe',
+                '*.dll',
+                '*.mp3',
+                '.md',
+                '*.ogg',
+                '*.flac',
+                '*.swp',
+                '*.swo',
+                '*.bmp',
+                '*.gif',
+                '*.ico',
+                '*.jpg',
+                '*.png',
+                '*.rar',
+                '*.zip',
+                '*.tar',
+                '*.tar.gz',
+                '*.tar.xz',
+                '*.tar.bz2',
+                '*.pdf',
+                '*.doc',
+                '*.docx',
+                '*.ppt',
+                '*.pptx',
+            }
+
+            vim.g.gutentags_add_default_project_roots = false
+            vim.g.gutentags_project_root = { 'package.json', '.git', '.svn', '.root', '.hg', '.vscode', '.project' }
+            vim.g.gutentags_ctags_tagfile = '.tags'
+
+            vim.g.gutentags_cache_dir = vim.fn.stdpath("data") .. '/ctags'
+            vim.g.gutentags_ctags_extra_args = { '--tag-relative=yes', '--fields=+ailmnS', '--extra=+q',
+                '--c++-kinds=+px', '--c-kinds=+px' }
+
+            -- custom
+            vim.g.gutentags_modules = { 'ctags' }
+
+            if vim.fn.isdirectory(vim.g.gutentags_cache_dir) == 0 then
+                os.execute("mkdir -p " .. vim.g.gutentags_cache_dir)
+            end
+        end
     },
     {
         "neovim/nvim-lspconfig",
